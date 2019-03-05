093c2da40dd559985c3993066b3ac68b5c12e46e
ultmaster <scottyugochang@hotmail.com>
2019-03-05 17:45:34 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-gunicorn app:app --workers 1 --worker-connections 1000 --error-logfile /app/gunicorn.log \
-    --timeout 600 --log-level warning --bind 0.0.0.0:20019
+gunicorn app:app --workers 1 --worker-connections 1000 --timeout 600 --log-level warning --bind 0.0.0.0:20019
