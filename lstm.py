34fbc6bb6f8096cc964785c722a60f1d04f8e665
zhangyuge <j9b73rsm#HGA>
2019-05-05 15:18:18 +0800

    lstm

--- lstm.py
+++ lstm.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -27,26 +29,34 @@ class Net(nn.Module):
 
 
 if __name__ == "__main__":
+  device = torch.device("cuda")
   problems, embeddings = get_problems_embeddings()
   submission_seqs = get_submission_sequences(problems)
   net = Net(embeddings.shape[1], embeddings.shape[0], torch.from_numpy(embeddings).float())
+  net.to(device)
   criterion = nn.CrossEntropyLoss()
   optimizer = optim.Adam(net.parameters(), lr=0.001)
 
   for epoch in range(200):
     print("Training on epoch %d" % epoch)
+    epoch_loss, epoch_acc = 0., 0.
+    counter = Counter()
     with ProgressBar(max_value=len(submission_seqs), redirect_stdout=True) as bar:
-      epoch_loss, epoch_acc = 0., 0.
-      for i, data in enumerate(submission_seqs):
+      for i, data_cpu in enumerate(submission_seqs):
         optimizer.zero_grad()
-        outputs = net(torch.tensor(data))
-        label_output = torch.tensor(data[1:])
+        data_gpu = torch.tensor(data_cpu).to(device)
+        outputs = net(data_gpu)
+        label_output = torch.tensor(data_cpu[1:]).to(device)
         loss = criterion(outputs, label_output)
         loss.backward()
         optimizer.step()
+
         epoch_loss += loss.item() / label_output.size()[0]
         _, predicted = torch.max(outputs.data, 1)
+        for x in predicted:
+          counter[x.item()] += 1
         epoch_acc += (predicted == label_output).sum().item() / label_output.size()[0]
         bar.update(i)
     print("Epoch loss: %.9f, Accuracy: %.9f" % (epoch_loss / len(submission_seqs),
                                                 epoch_acc / len(submission_seqs)))
+    print(counter)
